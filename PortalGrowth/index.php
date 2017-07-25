379228c69d25ce486d6e64aaa3643a87284ae7f8
Higor Palharini <higorpalharini@outlook.com>
2017-07-25 19:30:22 -0300

    adicionado script google analytics

--- PortalGrowth/index.php
+++ PortalGrowth/index.php
@@ -65,6 +65,17 @@
 				<button class ="btn btn-block btn-primary" ng-click="adicionarParticipante(participantesForm)" ng-disabled="formCadastroParticipante.$invalid" style="color:#FFFFF0">Quero me tornar um Growth Hacker</button>
 			</form>
 
+			<script>
+				  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+				  ga('create', 'UA-103200040-1', 'auto');
+				  ga('send', 'pageview');
+
+			</script>
+
 		</div>
 	</body>
 </html> 
