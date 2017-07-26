fda4372493e7c3260da108ec9c29b3e7c37b93b7
Higor Palharini <higorpalharini@outlook.com>
2017-07-25 23:36:41 -0300

    Alteração analytics2

--- PortalGrowth/index.php
+++ PortalGrowth/index.php
@@ -29,15 +29,6 @@
 	</head>
 	
 	<body ng-controller="participantesCtrl">
-
-		<script>
-			 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-			  ga('create', 'UA-103200040-1', 'auto');
-			  ga('send', 'pageview');
-		</script> 
 	
 		<div class ="jumbotron center" style="text-align: center; background-color:#30353B !important; ">
 
